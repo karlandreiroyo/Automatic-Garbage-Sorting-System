20e24973905c87b4b2d30256b663c2e47a906178
karlandreiroyo <karlandreiroyo86@gmail.com>
2026-03-28 17:48:45 +0800

    Update desktop_app.py

--- yolo/scripts/desktop_app.py
+++ yolo/scripts/desktop_app.py
@@ -6,9 +6,8 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path=Path(__file__).resolve().parent / ".env")
-
 
 class DesktopApp:
     def __init__(self):
+        load_dotenv(dotenv_path=Path(__file__).resolve().parent / ".env")
         self.ws_url = os.getenv("WS_URL", "ws://localhost:3001")
